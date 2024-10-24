d3f173ca2193f8fb9c849f7a5ef1a0e668e9d992
softoasis2022 <132141967+softoasis2022@users.noreply.github.com>
2024-10-24 20:57:41 +0900

    Delete js/route/database.js

--- js/route/database.js
+++
@@ -1,9 +0,0 @@
-const db = require('mysql')
- const conn = db.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'user',
-  password: 'user',
-  database: 'user'
- })
- module.exports = conn
